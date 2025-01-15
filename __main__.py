35b46a0fd6bd4b817516422061dc77e768d38576
Pedro Gomes Moreira <p.gomesmoreira@student.tudelft.nl>
2025-01-15 15:53:57 +0100

    Finished basic version of bit string transmission

--- __main__.py
+++ __main__.py
@@ -5,7 +5,8 @@ from qiskit_aer import AerSimulator
 import circuit
 
 from matplotlib import pyplot as plt
-from qiskit_aer import AerSimulator
+
+from circuit import transmit_image
 
 import matplotlib
 
@@ -14,7 +15,7 @@ import matplotlib
 
 def main():
 
-    send_image()
+    transmit_image()
     return
 
     # Create a quantum circuit with two qubits
@@ -27,44 +28,6 @@ def main():
 
     plot_histogram(counts, title="Superdense coding")
     plt.show()
-
-def send_image():#img: Image):
-    #bitstr = img.to_bistring()
-    bitstr = "001100110110"
-
-    # We transmit two bits at a time   
-    # For now, assume that bitstr is even, for simplicity
-    n = int(len(bitstr) / 2)
-
-    circuits = [QuantumCircuit(2, 2) for i in range(n)]
-
-    # First, entangle `n` qubits
-    for circ in circuits:
-        circ.h(0)
-        circ.cx(0, 1)
-        circ.barrier()
-
-    # Here we can wait for an indeterminate amount of time
-
-    # Alice applies operations to each of her qubit depending on the bit pair
-    for i in range(n):
-        circ = circuits[i]
-
-        msg = bitstr[2*i:2*(i+1)]
-        encode_message(msg, circ)
-        
-        # Now, Alice sends her component of the entangled pair
-        # And Bob receives and decodes it
-
-        circ.cx(0, 1)
-        circ.h(1)
-        circ.measure_all()
-    
-        # Simulate circuit
-        result = AerSimulator().run(circ).result()
-
-        # Then do something with the result (https://docs.quantum.ibm.com/api/qiskit/qiskit.result.Result)
-        print (result.get_counts().most_frequent())
 
 
 if __name__ == "__main__":
--- circuit.py
+++ circuit.py
@@ -1,4 +1,5 @@
 from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
 
 
 def encode_message(circuit, msg, index=0):
@@ -39,3 +40,47 @@ def build_circuit(message):
     circuit.measure_all()
 
     return circuit
+
+def transmit_image():
+    bitstr = "001101101010010010"
+
+    print(f"Alice transmits bit string\t{bitstr}")
+
+    # We transmit two bits at a time   
+    # For now, assume that bitstr is even, for simplicity
+    n = int(len(bitstr) / 2)
+
+    circuits = [QuantumCircuit(2) for i in range(n)]
+
+    # First, entangle `n` qubits
+    for circ in circuits:
+        circ.h(0)
+        circ.cx(0, 1)
+        circ.barrier()
+
+    # Here we can wait for an arbitrary amount of time
+
+    # Bistring measured by Bob
+    bob_bitstr = ""
+
+    # Alice applies operations to each of her qubit depending on the bit pair
+    for i in range(n):
+        circ = circuits[i]
+
+        msg = bitstr[2*i:2*(i+1)]
+        encode_message(circ, msg)
+        
+        # Now, Alice sends her component of the entangled pair
+        # And Bob receives and decodes it
+
+        circ.cx(0, 1)
+        circ.h(0)
+        circ.measure_all()
+    
+        # Simulate circuit
+        result = AerSimulator().run(circ).result()
+
+        # Then append the measurement result to the string
+        bob_bitstr += result.get_counts().most_frequent()
+    
+    print(f"Bob reads bit string\t\t{bob_bitstr}")
