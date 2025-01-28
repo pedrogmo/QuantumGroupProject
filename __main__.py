7ffcb64fa6b20281e7a741a092a3871d236f1957
Kajetan Neumann <k.m.neumann@student.tudelft.nl>
2025-01-28 22:45:40 +0100

    added funct to run all possible 2 bit mappings

--- __main__.py
+++ __main__.py
@@ -105,7 +105,32 @@ def mapped_image():
         transmit_msg(mapping, compress_flag=False)
 
 
+def mapped_image_all(compress_flag: bool = False, image="./images/mario.png"):
+    """
+    Transmits an image using dense coding.
+
+    :param mapping: bit-mapping to go over image (2 bit string)
+    :param compress_flag: whether to compress the image or not
+    :param image: the image path to transmit
+    """
+    simulator = AerSimulator()
+    image = Image(image)
+    message = image.to_bitstr(compress_flag=compress_flag, encoding="RGB")
+
+    mappings = ["00", "01", "10", "11"]
+    for i in range(len(mappings)):
+        print("Transmitting message", i, "of length ", len(message))
+        message_result = simulation.simulate_normal(simulator, message, 28, 1, mapping=mappings[i])[0]
+        image_result = Image.from_bitstr(message_result, image.width, image.height, compress_flag=compress_flag, encoding="RGB")
+        image_result.display(subplot=(2, 2, i+1))
+        print("Finished transmitting image!")
+
+    plt.show()
+
+
+
 if __name__ == "__main__":
     # main()
     # transmit_msg()
-    mapped_image()
+    # mapped_image()
+    mapped_image_all()
